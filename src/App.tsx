68c0ad8070c76c13e269de33a92d65331cc7b392
Rostyslav Pudlo <rostyslav.pudlo@gmail.com>
2024-07-26 21:35:53 +0300

    add base actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,52 @@
 import { useState } from 'react'
 import './App.css'
 
+let nextId = 0;  // TODO: temporary code; remove after back-end using
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState<{id: number}[]>([]);
+
+  function createClick() {
+    setTasks(
+      [
+        ...tasks,
+        { id: nextId++ }
+      ]
+    );
+  }
+
+  function refreshClick() {
+  }
+
+  function stopClick() {
+  }
+
+  function removeClick() {
+  }
+
+  const tasksItems = Object.values(tasks)
+  .map((task) => (
+    <tr>
+      <td>{task.id}</td>
+      <td>
+        <button type="button" className="btn btn-primary" onClick={refreshClick}>refresh</button>
+        <button type="button" className="btn btn-primary" onClick={stopClick}>stop</button>
+        <button type="button" className="btn btn-primary" onClick={removeClick}>remove</button>
+      </td>
+    </tr>
+  ))
 
   return (
     <>
-      <button type="button" className="btn btn-primary" onClick={()=>{}}>create task</button>
+      <button type="button" className="btn btn-primary" onClick={createClick}>create task</button>
       <table className="table">
         <thead>
           <tr>
             <th>id</th>
-            <th>status</th>
-            <th>refresh</th>
           </tr>
         </thead>
-        <tbody id="tasks">
+        <tbody>
+          {tasksItems}
         </tbody>
       </table>
     </>
