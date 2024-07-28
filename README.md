2e7a9a75d735635da673a65d2d8d785a7320de85
Rostyslav Pudlo <rostyslav.pudlo@gmail.com>
2024-07-28 22:49:44 +0300

    add crud operations

--- README.md
+++ README.md
@@ -2,29 +2,31 @@
 background tasks manager
 
 As a user
-I want to have a front-end tool to manage background tasks
-So that I can better organize my work
+I want to have a front-end tool to manage long term actions (background tasks on server)
+so that I can better organize my work
 
 ## issues
-- multiuser.
-- monitor my tasks.
-- remove my task.
-- subscribe to task. if I start an duplicate of existed task than subscribe to task.
-- unsubscribe of task.
-- show number of task in order.
-- refresh task info manually.
-- refresh tasks info manually.
-- refresh task info by using timeout.
-- refresh tasks info by using timeout.
-- refresh tasks info by using websocket.
-- show results of tasks.
-- remove results of tasks.
-- start task.
-- create duplicate of command to edit and start task.
-- show a progress of task; show time to finish.
-- use celery as bakground tasks.
-- use fastapi background task (if it is possible).
-- use dramtiq as background tasks.
+issue 1. multiuser.
+issue 2. view my tasks. task information has `id`, `name`, `description`, `state`, `error` (if it is present and what to do), `link` (to page which relates to or has run the task)
+issue 3. remove my task. clear unimportant information.
+issue 4. do not create task if there is not finished task in query of current or other user with the same name and arguments. add existed task to user tasks.
+issue 5. do not delete task if there are several users which create task with equal arguments.
+issue 6. show number of task in order.
+issue 7. refresh task info manually.
+issue 8. refresh tasks info manually.
+issue 9. refresh task info by using timeout.
+issue 10. refresh tasks info by using timeout.
+issue 11. refresh tasks info by using websocket.
+issue 12. show error of task if it exists.
+issue 13. remove results of tasks.
+issue 14. start task.
+issue 15. create duplicate of command to edit and start task.
+issue 16. show a progress of task; show time to finish.
+issue 17. use celery for bakground tasks.
+issue 18. use fastapi background task (if it is possible).
+issue 19. use dramtiq for background tasks.
+issue 20. group duplicated tasks has be run before. show only the last and group.
+issue 21. ...
 
 ## references
 - [Asynchronous Tasks with FastAPI and Celery | TestDriven.io](https://testdriven.io/blog/fastapi-and-celery/). it is an example to start.
--- src/App.tsx
+++ src/App.tsx
@@ -4,45 +4,109 @@ import './App.css'
 let nextId = 0;  // TODO: temporary code; remove after back-end using
 
 function App() {
-  const [tasks, setTasks] = useState<{id: number}[]>([]);
+  const [tasks, setTasks] = useState<{id: number, name: string, description: string, state: string, link: string}[]>([]);
 
-  function createClick() {
+  function sendCreateTask(ids?: number[]) {
+    // TODO: temporary mock
+    if (!ids) {
+      nextId++;
+      return [{ id: nextId, name: `name${nextId}`, description: `description${nextId}`, state: "PENDING", link: `link${nextId}` }]
+    }
+
+    // recreate task
+    return tasks.filter(task => ids.includes(task.id)).map(task => {
+      nextId++;
+      return { ...task, id: nextId, state: "PENDING" }
+    })
+  }
+
+  function sendReadTasks(ids?: number[]) {
+    // TODO: temporary mock
+    if (!ids) {
+      return tasks.map(task => { return {...task, state: "SUCCESS"} })
+    }
+
+    return tasks.filter(task => ids.includes(task.id)).map(task => { return {...task, state: "SUCCESS"} })
+  }
+
+  function sendUpdateTasks(ids?: number[]) {
+    // TODO: temporary mock
+    if (!ids) {
+      return tasks.map(task => { return {...task, state: "REVOKED"} })
+    }
+
+    return tasks.filter(task => ids.includes(task.id)).map(task => { return {...task, state: "REVOKED"} })
+  }
+
+  function sendDeleteTasks(ids?: number[]) {
+    // TODO: temporary mock
+    ids = ids
+  }
+
+
+  function createTasks(ids?: number[]) {
+    const newTasks = sendCreateTask(ids)
+    setTasks([ ...tasks, ...newTasks ]);
+  }
+
+  function readTasks(ids?: number[]) {
+    const newTasks = sendReadTasks(ids)
+
     setTasks(
-      [
-        ...tasks,
-        { id: nextId++ }
-      ]
-    );
+      ids? tasks.filter(task => !ids.includes(task.id)).concat(newTasks): newTasks  // TODO: optimize
+    )
   }
 
-  function refreshClick() {
+  // stop task
+  function updateTasks(ids?: number[]) {
+    const newTasks = sendUpdateTasks(ids)
+
+    setTasks(
+      ids? tasks.filter(task => !ids.includes(task.id)).concat(newTasks): newTasks  // TODO: optimize
+    )
   }
 
-  function stopClick() {
-  }
+  function deleteTasks(ids?: number[]) {
+    sendDeleteTasks(ids);
 
-  function removeClick() {
+    setTasks(
+      ids? tasks.filter(task => !ids.includes(task.id)): []  // TODO: optimize
+    )
   }
 
   const tasksItems = Object.values(tasks)
+  .sort((a, b) => a.id - b.id)
   .map((task) => (
     <tr>
       <td>{task.id}</td>
+      <td>{task.name}</td>
+      <td>{task.description}</td>
+      <td>{task.state}</td>
+      <td>{task.link}</td>
       <td>
-        <button type="button" className="btn btn-primary" onClick={refreshClick}>refresh</button>
-        <button type="button" className="btn btn-primary" onClick={stopClick}>stop</button>
-        <button type="button" className="btn btn-primary" onClick={removeClick}>remove</button>
+        <button type="button" className="btn btn-primary mr-1" onClick={() => createTasks([task.id])}>recreate task</button>
+        <button type="button" className="btn btn-primary mr-1" onClick={() => readTasks([task.id])}>update task info</button>
+        <button type="button" className="btn btn-warning mr-1" onClick={() => updateTasks([task.id])}>stop task</button>
+        <button type="button" className="btn btn-danger mr-1" onClick={() => deleteTasks([task.id])}>delete task</button>
       </td>
     </tr>
   ))
 
   return (
     <>
-      <button type="button" className="btn btn-primary" onClick={createClick}>create task</button>
+      <button type="button" className="btn btn-primary mr-1" onClick={() => createTasks()}>create task</button>
+      <button type="button" className="btn btn-primary mr-1" onClick={() => readTasks()}>update tasks info</button>
+      <button type="button" className="btn btn-warning mr-1" onClick={() => updateTasks()}>stop tasks</button>
+      <button type="button" className="btn btn-danger mr-1" onClick={() => deleteTasks()}>delete tasks</button>
       <table className="table">
         <thead>
           <tr>
             <th>id</th>
+            <th>name</th>
+            <th>description</th>
+            <th>state</th>
+            <th>link</th>
+            <th>actions</th>
           </tr>
         </thead>
         <tbody>
